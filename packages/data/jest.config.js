9890dba5537871e89f860db36cf4a5d6399e6993
Kassem Shehady <k.shehady.ai@gmail.com>
2026-01-30 09:25:39 +0200

    update jest and tsconfig for improved module resolution and test matching

--- packages/data/jest.config.js
+++ packages/data/jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['<rootDir>/tests/**/*.test.ts'],
+  testPathIgnorePatterns: ['<rootDir>/dist/'],
+  moduleNameMapper: {
+    '^@socialpublisher/core$': '<rootDir>/../core/src',
+  },
   //setupFiles: ['<rootDir>/tests/setup.ts'],
 };
